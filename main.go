9c223ee66b50aeeb206c0f16544d578777368fd1
HarrisonLavin <harrison.lavin@gmail.com>
2018-02-09 18:46:39 -0500

    Hello World

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/kataras/iris"
+
+func main() {
+	app := iris.New()
+
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.HTML("<h1>Hello World</h1>")
+	})
+
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
